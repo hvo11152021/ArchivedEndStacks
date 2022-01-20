1b199ec896a207f1c234141aecee7dfbcb3e04ce
Hy Vo <94402902+hvo11152021@users.noreply.github.com>
2022-01-20 16:23:42 -0500

    Build Views for CA Games

---
+++ HyVo_CanadaGames/HyVo_CanadaGames/Models/AthleteSport.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HyVo_CanadaGames.Models
+{
+    public class AthleteSport
+    {
+        public int SportID { get; set; }
+        public Sport Sport { get; set; }
+
+        public int AthleteID { get; set; }
+        public Athlete Athlete { get; set; }
+
+    }
+}
---
+++ HyVo_CanadaGames/HyVo_CanadaGames/Ultilities/PaginatedList.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HyVo_CanadaGames.Ultilities
+{
+    public class PaginatedList<T> : List<T>
+    {
+        public int PageIndex { get; private set; }
+        public int TotalPages { get; private set; }
+
+        public PaginatedList(List<T> items, int count, int pageIndex, int pageAmount)
+        {
+            PageIndex = pageIndex;
+            TotalPages = (int)Math.Ceiling(count / (double)pageAmount);
+            this.AddRange(items);
+        }
+
+        public bool HasPreviousPage
+        {
+            get => PageIndex > 1;
+        }
+
+        public bool HasNextPage
+        {
+            get => PageIndex < TotalPages;
+        }
+
+        public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageAmount)
+        {
+            var count = await source.CountAsync();
+            var items = await source
+                .Skip((pageIndex - 1) * pageAmount)
+                .Take(pageAmount)
+                .ToListAsync();
+            return new PaginatedList<T>(items, count, pageIndex, pageAmount);
+        }
+    }
+}
