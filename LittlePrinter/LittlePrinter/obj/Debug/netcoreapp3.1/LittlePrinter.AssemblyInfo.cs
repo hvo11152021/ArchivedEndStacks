873f227af6b398e7870c65900725848be68549cf
Hy Vo <94402902+hvo11152021@users.noreply.github.com>
2022-05-12 20:43:06 -0500

    Build tag builder views

--- LittlePrinter/LittlePrinter/obj/Debug/netcoreapp3.1/LittlePrinter.AssemblyInfo.cs
+++ LittlePrinter/LittlePrinter/obj/Debug/netcoreapp3.1/LittlePrinter.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LittlePrinter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f12d55b91cba4de92cce6a2a6ef3969b6d3ffec3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b0bf089e6b528910306d67cd5766db5abced114")]
 [assembly: System.Reflection.AssemblyProductAttribute("LittlePrinter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LittlePrinter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
