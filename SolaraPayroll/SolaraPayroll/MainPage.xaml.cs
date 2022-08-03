55e193bfedbe868c7fce1fea9293dc05ecb1fe10
Hy Vo <94402902+hvo11152021@users.noreply.github.com>
2022-08-03 15:49:23 -0500

    Main Page variables and properties

--- SolaraPayroll/SolaraPayroll/MainPage.xaml.cs
+++ SolaraPayroll/SolaraPayroll/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BusinessLogic;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -22,9 +23,65 @@ namespace SolaraPayroll
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private List<Employee> empList = Data.GetDataRecords();
+        private List<Employee> payrollList = new List<Employee>();
+        private Employee selectedPerson;
+        private List<string> prOutput = new List<string>();
+
+        List<string> tempEmpList = new List<string>();
+
+        private string EmpName { get; set; }
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            txtFirst.IsEnabled = false;
+            txtLast.IsEnabled = false;
+            txtSiN.IsEnabled = false;
+            dtpInputDOB.IsEnabled = false;
+            txtPhone.IsEnabled = false;
+            txtStreet.IsEnabled = false;
+            txtCity.IsEnabled = false;
+            txtProvince.IsEnabled = false;
+            txtZip.IsEnabled = false;
+            txtEmail.IsEnabled = false;
+
+            cboEmployeeType.IsEnabled = false;
+            txtSalary.IsEnabled = false;
+            txtHourWorked.IsEnabled = false;
+            txtHourlyRate.IsEnabled = false;
+            btnUpdate.IsEnabled = false;
+            btnAddNew.IsEnabled = false;
+            btnCancel.IsEnabled = false;
+
+            btnNewpayroll.IsEnabled = true;
+            dtpkPayDate.IsEnabled = false;
+            btnPRSubmit.IsEnabled = false;
+
+            btnAddNew.Visibility = Visibility.Collapsed;
+
+            cboEmpType.ItemsSource = new string[] { "Hourly", "Salary", "Software Developer", "Supply Manager" };
+            lvEmpList.Items.Clear();
+            lvEmpList.ItemsSource = empList;
+            foreach (Employee emp in empList)
+            {
+                tempEmpList.Add(emp.FirstName);
+            }
+
+            lvStatements.ItemsSource = payrollList;
+
+            cboFilterOptions.ItemsSource = new string[]
+            {
+                "Employee Types",
+                "Activities",
+                "Seniority",
+                "Highest Pay",
+                "Highest Pay Type",
+                "Average Pay",
+                "Highest Bonus"
+            };
+
         }
     }
 }
