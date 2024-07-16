967bbe81801f5a8199983fccb5337eca8a46ee61
Hy Vo <94402902+hvo11152021@users.noreply.github.com>
2024-07-15 20:26:09 -0500

    Check 30 or Sum = 30

--- C#BasicAlgorithms/Program.cs
+++ C#BasicAlgorithms/Program.cs
@@ -21,22 +21,36 @@ namespace C_BasicAlgorithms
         //    return x == y ? (x + y) * 3 : x + y;
         //}
 
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine(test(53));
+        //    Console.WriteLine(test(30));
+        //    Console.WriteLine(test(51));
+        //    Console.ReadLine();
+        //}
+
+        //public static int test(int n)
+        //{
+        //    const int x = 51;
+        //    if (n > x)
+        //    {
+        //        return (n - x) * 3;
+        //    }
+        //    return x - n;
+        //}
+
         static void Main(string[] args)
         {
-            Console.WriteLine(test(53));
-            Console.WriteLine(test(30));
-            Console.WriteLine(test(51));
+            Console.WriteLine(test(30, 0));
+            Console.WriteLine(test(25, 5));
+            Console.WriteLine(test(20, 30));
+            Console.WriteLine(test(20, 25));
             Console.ReadLine();
         }
 
-        public static int test(int n)
+        public static bool test(int x, int y)
         {
-            const int x = 51;
-            if (n > x)
-            {
-                return (n - x) * 3;
-            }
-            return x - n;
+            return x == 30 || y == 30 || (x + y == 30);
         }
     }
 }
