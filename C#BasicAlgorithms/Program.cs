4768b61e546808d93acc8bf59b41e089634e7012
Hy Vo <94402902+hvo11152021@users.noreply.github.com>
2024-07-15 20:12:02 -0500

    Absolute difference between n and 51

--- C#BasicAlgorithms/Program.cs
+++ C#BasicAlgorithms/Program.cs
@@ -8,17 +8,35 @@ namespace C_BasicAlgorithms
 {
     internal class Program
     {
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine(test(1, 2));
+        //    Console.WriteLine(test(3, 2));
+        //    Console.WriteLine(test(2, 2));
+        //    Console.ReadLine();
+        //}
+
+        //public static int test(int x, int y)
+        //{
+        //    return x == y ? (x + y) * 3 : x + y;
+        //}
+
         static void Main(string[] args)
         {
-            Console.WriteLine(test(1, 2));
-            Console.WriteLine(test(3, 2));
-            Console.WriteLine(test(2, 2));
+            Console.WriteLine(test(53));
+            Console.WriteLine(test(30));
+            Console.WriteLine(test(51));
             Console.ReadLine();
         }
 
-        public static int test(int x, int y)
+        public static int test(int n)
         {
-            return x == y ? (x + y) * 3 : x + y;
+            const int x = 51;
+            if (n > x)
+            {
+                return (n - x) * 3;
+            }
+            return x - n;
         }
     }
 }
