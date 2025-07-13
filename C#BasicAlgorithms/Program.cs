5298487ba8b82b61a30968e21019199f17a35840
Hy Vo <94402902+hvo11152021@users.noreply.github.com>
2025-07-12 21:36:44 -0500

    Four Copies of First Two Characters

--- C#BasicAlgorithms/Program.cs
+++ C#BasicAlgorithms/Program.cs
@@ -97,19 +97,34 @@ namespace C_BasicAlgorithms
         //    return str.Remove(n, 1);
         //}
 
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine(test("abcd"));
+        //    Console.WriteLine(test("a"));
+        //    Console.WriteLine(test("xy"));
+        //    Console.ReadLine();
+        //}
+
+        //public static string test(string str)
+        //{
+        //    return str.Length > 1
+        //        ? str.Substring(str.Length - 1) + str.Substring(1, str.Length - 2) + str.Substring(0, 1)
+        //        : str;
+        //}
+
         static void Main(string[] args)
         {
-            Console.WriteLine(test("abcd"));
+            Console.WriteLine(test("C Sharp"));
+            Console.WriteLine(test("JS"));
             Console.WriteLine(test("a"));
-            Console.WriteLine(test("xy"));
             Console.ReadLine();
         }
 
         public static string test(string str)
         {
-            return str.Length > 1
-                ? str.Substring(str.Length - 1) + str.Substring(1, str.Length - 2) + str.Substring(0, 1)
-                : str;
+            return str.Length < 2
+                ? str
+                : str.Substring(0, 2) + str.Substring(0, 2) + str.Substring(0, 2) + str.Substring(0, 2);
         }
     }
 }
